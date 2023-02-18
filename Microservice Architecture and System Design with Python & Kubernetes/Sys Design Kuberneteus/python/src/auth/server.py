6ce3925d57635616cfb73e3e09de299156fc3173
Danylo Berdnyk <berdstudy@gmail.com>
2023-02-18 14:06:41 +0200

    adding JWT tokens to our Auth flow

--- Microservice Architecture and System Design with Python & Kubernetes/Sys Design Kuberneteus/python/src/auth/server.py
+++ Microservice Architecture and System Design with Python & Kubernetes/Sys Design Kuberneteus/python/src/auth/server.py
@@ -17,6 +17,7 @@ server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
+
 @server.route("/login", methods=["POST"])
 def login():
     """
@@ -53,3 +54,54 @@ def login():
     else:
         return "invalid credentials", 401
 
+
+@server.route("/validate", metods=["POST"])
+def validate():
+    """
+    Gets jwt token in Authorization header, takes <credentials> part from jwt and decodes with secret key
+    returns "not authorized", 403 in case of wrong key, otherwise returns decoded jwt and 200 code
+    """
+
+    # jwt token should be in Authorization header
+    encoded_jwt = request.headers['Authorization']
+
+    # if jwt is not present in header return error
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    # We require our jwt be in format <type> <credentials>, specifically Bearer <credentials>
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    # decoding with secret key
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+
+def createJWT(username, secret, authz: bool):
+    """
+    Fuction accepts claims of the user, private key, and if the user is admin
+    Returns Jwt token based on the info
+    """
+    return jwt.encode(
+            payload={
+                "username": username,
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1), # set expiration of token 1 day
+                "iat": datetime.datetime.utcnow(), # when the token is issued
+                "admin": authz,
+            },
+            key=secret,
+            algorithm="HS256",
+            )
+
+
+if __name__ == "__main__":
+    # host 0.0.0.0 allows to listen to any address available on our host (to all public IPs), the default is localhost
+    # the host is our docker container and it can have different if addresses within docker network so we use 0.0.0.0 
+    # using local host, requests sent from outside of conitainer will never make to our flask app
+    server.run(host="0.0.0.0", port=5000)
